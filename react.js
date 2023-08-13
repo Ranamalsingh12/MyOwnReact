574488ffeb16446ada9ed6365874ab3a0bd8588d
Ranamalsingh12 <ranams99911@gmail.com>
2023-08-13 14:46:21 +0530

    making change in the child node

--- react.js
+++ react.js
@@ -1,13 +1,13 @@
 (() => {
 
-    function div(children) {
-        const anEle = document.createElement('div');
+    function div(el,children) {
+        const anEle = document.createElement(el);
         anEle.innerHTML = children.join(' ');
         return anEle
     }
 
     function createElement(el, props, ...children) {
-        return div(children);
+        return div(el,children);
       }
 
     window.React = {
