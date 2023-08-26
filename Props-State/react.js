c345bff2370e0adf35e9ae7e37e9f7ede507011e
Ranamalsingh12 <ranams99911@gmail.com>
2023-08-26 13:43:58 +0530

    state and props

--- Props-State/react.js
+++ Props-State/react.js
@@ -1,33 +1,3 @@
-// (() => {
-
-// const { func } = require("prop-types");
-
-//     function anElement(el,children) {
-//         if(typeof(el) === 'function') {
-//             return el();
-//         } else {
-//             const anEle = document.createElement(el);
-//             anEle.innerHTML = children.join(' ');
-//             return anEle;
-//         }
-//     }
-
-//     function createElement(el, props, ...children) {
-//         return anElement(el,children);
-//       }
-
-//     window.React = {
-//         createElement
-//     };
-
-//     window.ReactDOM = {
-//         render: (el, domEL) => {
-//             domEL.appendChild(el);
-//         }
-//     };
-
-// }) ();
-
 (() => {
 
     function anElement(el,props,children) {
